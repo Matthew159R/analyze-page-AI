c88261f1041811b820c1e656f8a1c3b56d154c56
devMatheus <dartboots159@gmail.com>
2024-03-02 17:48:28 -0300

    app.js start application

---
+++ app.js
@@ -0,0 +1,13 @@
+const scraping = require('./scrapingModule');
+const sendMessageToAIModel = require('./sendMessageToAiModule');
+
+scraping('main')
+    .then(data => {
+        const contentWebSite = JSON.stringify(data)
+        const userExpectation = 'Desejo aprender a fazer sistemas de login robustos com jwt';
+        const userExpectation2 = 'Desejo iniciar em spring'
+        sendMessageToAIModel(userExpectation2, contentWebSite)
+            .then(responseAi => console.log(responseAi))
+            .catch(error => console.error('Ocorreu um erro na reposta do modelo de ai', error));
+    })
+    .catch(error => console.error('Erro ao fazer o web scraping', error));
